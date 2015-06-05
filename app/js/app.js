5ad00f0dfa534ac2f528b7df352c30d5b1c1eb90
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2015-06-05 12:12:55 +0700

    add smooth screen

--- app/js/app.js
+++ app/js/app.js
@@ -14,6 +14,34 @@ var app = (function(document, $) {
                 $(document).foundation('joyride', 'start');
             });
 			_userAgentInit();
+
+			/*
+			Custom JS
+			*/
+			// Filter portfolio
+			$('.portfolio').mixItUp();
+
+			// Simple smooth scroll function
+			var smoothScroll = function() {
+
+			}
+
+			// Simple Smooth scroll
+			var link = $('.jumbotron__arrow-down, .top-bar-section a');
+			link.on('click', function() {
+				var target = $(this).attr('href');
+
+				$('html, body').animate({
+					scrollTop: $(target).offset().top
+				}, 800);
+
+				return false;
+			});
+
+			/*
+			End Custom JS
+			*/
+
 		};
 	return {
 		init: _init
@@ -22,52 +50,48 @@ var app = (function(document, $) {
 
 (function() {
 	app.init();
+})();
 
-	// Filter portfolio
-	$('.portfolio').mixItUp();
-
-	/*
+/*
 
 Google Map
 
 */
 
-	function initialize() {
-	  var mapOptions = {
-	    zoom: 17,
-	    scrollwheel: false,
-	    // draggable: false,
-	    center: {lat: 45.478135, lng: 9.123812} 
-	  };
+function initialize() {
+  var mapOptions = {
+    zoom: 17,
+    scrollwheel: false,
+    // draggable: false,
+    center: {lat: 45.478135, lng: 9.123812} 
+  };
 
-	  var map = new google.maps.Map(document.getElementById('map-canvas'),
-	    mapOptions);
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+    mapOptions);
 
-	  infowindow = new google.maps.InfoWindow();
+  infowindow = new google.maps.InfoWindow();
 
-	  var marker = new google.maps.Marker({
-	    map: map,
-	    // Define the place with a location, and a query string.
-	    place: {
-	      location: {lat: 45.478135, lng: 9.123812},
-	      query: 'San Siro Stadium'
+  var marker = new google.maps.Marker({
+    map: map,
+    // Define the place with a location, and a query string.
+    place: {
+      location: {lat: 45.478135, lng: 9.123812},
+      query: 'San Siro Stadium'
 
-	    },
-	    // Attributions help users find your site again.
-	    attribution: {
-	      source: 'Google Maps JavaScript API',
-	      webUrl: 'https://developers.google.com/maps/'
-	    }
-	  });
+    },
+    // Attributions help users find your site again.
+    attribution: {
+      source: 'Google Maps JavaScript API',
+      webUrl: 'https://developers.google.com/maps/'
+    }
+  });
 
-	  // Construct a new InfoWindow.
-	  var infowindow = new google.maps.InfoWindow({
-	    content: 'Creative Company'
-	  });
+  // Construct a new InfoWindow.
+  var infowindow = new google.maps.InfoWindow({
+    content: 'Creative Company'
+  });
 
-	  infowindow.open(map, marker);
-	}
+  infowindow.open(map, marker);
+}
 
-	google.maps.event.addDomListener(window, 'load', initialize);
-	
-})();
+google.maps.event.addDomListener(window, 'load', initialize);

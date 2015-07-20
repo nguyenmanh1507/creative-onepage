5ccd8e3a4ac1d803e7621d98de1c016cf24b9053
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2015-07-20 15:35:06 +0700

    update style for google map

--- app/js/app.js
+++ app/js/app.js
@@ -157,15 +157,27 @@ Google Map
 */
 
 function initialize() {
+	// Create array of styler
+	var styles = [{'featureType':'all','elementType':'all','stylers':[{'visibility':'simplified'},{'saturation':'-100'},{'invert_lightness':true},{'lightness':'11'},{'gamma':'1.27'}]},{'featureType':'administrative.locality','elementType':'all','stylers':[{'visibility':'off'}]},{'featureType':'landscape.man_made','elementType':'all','stylers':[{'hue':'#ff0000'},{'visibility':'simplified'},{'invert_lightness':true},{'lightness':'-10'},{'gamma':'0.54'},{'saturation':'45'}]},{'featureType':'poi.business','elementType':'all','stylers':[{'visibility':'simplified'},{'hue':'#ff0000'},{'saturation':'75'},{'lightness':'24'},{'gamma':'0.70'},{'invert_lightness':true}]},{'featureType':'poi.government','elementType':'all','stylers':[{'hue':'#ff0000'},{'visibility':'simplified'},{'invert_lightness':true},{'lightness':'-24'},{'gamma':'0.59'},{'saturation':'59'}]},{'featureType':'poi.medical','elementType':'all','stylers':[{'visibility':'simplified'},{'invert_lightness':true},{'hue':'#ff0000'},{'saturation':'73'},{'lightness':'-24'},{'gamma':'0.59'}]},{'featureType':'poi.park','elementType':'all','stylers':[{'lightness':'-41'}]},{'featureType':'poi.school','elementType':'all','stylers':[{'visibility':'simplified'},{'hue':'#ff0000'},{'invert_lightness':true},{'saturation':'43'},{'lightness':'-16'},{'gamma':'0.73'}]},{'featureType':'poi.sports_complex','elementType':'all','stylers':[{'hue':'#ff0000'},{'saturation':'43'},{'lightness':'-11'},{'gamma':'0.73'},{'invert_lightness':true}]},{'featureType':'road','elementType':'all','stylers':[{'saturation':'45'},{'lightness':'53'},{'gamma':'0.67'},{'invert_lightness':true},{'hue':'#ff0000'},{'visibility':'simplified'}]},{'featureType':'road','elementType':'labels','stylers':[{'visibility':'off'}]},{'featureType':'transit','elementType':'all','stylers':[{'visibility':'simplified'},{'hue':'#ff0000'},{'saturation':'38'},{'lightness':'-16'},{'gamma':'0.86'}]}];
+
+	var styledMap = new google.maps.StyledMapType(styles, {name: 'Styled Map'});
+
   var mapOptions = {
-    zoom: 17,
+    zoom: 16,
     scrollwheel: false,
     // draggable: false,
-    center: {lat: 45.478135, lng: 9.123812} 
+    center: {lat: 45.478135, lng: 9.123812},
+    mapTypeContronlOptions: {
+    	mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+    }
   };
 
   var map = new google.maps.Map(document.getElementById('map-canvas'),
     mapOptions);
+
+  //Associate the styled map with the MapTypeId and set it to display.
+  map.mapTypes.set('map_style', styledMap);
+  map.setMapTypeId('map_style');
 
   infowindow = new google.maps.InfoWindow();
 
